d398eaa127602e7c08c92033f866ba3bfc4ff8d9
Max Bernstein <emacs@meta.com>
2022-12-12 11:53:15 -0800

    wip

--- util/macos.cmake
+++ util/macos.cmake
@@ -3,12 +3,17 @@ set(SYSCONFIGDATA ${CMAKE_CURRENT_LIST_DIR}/linux/_sysconfigdata__linux_.py)
 
 find_package(openssl REQUIRED)
 
+set(PLATFORM_COMPILER_FLAGS -Wno-error=deprecated-declarations)
+
+set(XZ_INCLUDE_DIRS "/Users/emacs/.local/homebrew/opt/xz/include")
+set(OPENSSL_INCLUDE_DIRS "/Users/emacs/.local/homebrew/opt/openssl/include")
+
 set(BZIP2_LIBRARIES bz2)
 set(FFI_LIBRARIES ffi)
 set(NCURSES_LIBRARIES ncurses tinfo)
 set(OPENSSL_PREFIX "/Users/emacs/.local/homebrew/opt")
 set(OPENSSL_LIBRARIES crypto ssl)
 set(READLINE_LIBRARIES readline)
-set(SQLITE_LIBRARIES "/Users/emacs/.local/homebrew/opt")
-set(XZ_LIBRARIES "/Users/emacs/.local/homebrew/opt/xz")
+set(SQLITE_LIBRARIES sqlite3)
+set(XZ_LIBRARIES xz)
 set(ZLIB_LIBRARIES z)
