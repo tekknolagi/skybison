6d5a51e10a894f077a672e9d15917902663dab5c
Max Bernstein <max@bernsteinbear.com>
2023-03-23 10:42:53 -0400

    Upgrade run() to more advanced version
    This is definitely also used somewhere else in the project and I do not
    remember where.

--- benchmarks/quickbench/util.py
+++ benchmarks/quickbench/util.py
@@ -2,24 +2,58 @@ import logging
 import os
 import subprocess
 import sys
-
+import shlex
+import textwrap
 
 SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
-def run(cmd, check=True, encoding="utf-8", log_level=logging.DEBUG, **kwargs):
-    if "cwd" in kwargs:
-        log_msg = f"$ cd {kwargs['cwd']}; {' '.join(cmd)}"
-    else:
-        log_msg = f"$ {' '.join(cmd)}"
-    if not kwargs.get('stdout'):
-        # Only pipe stdout; we want stderr (logging, errors) to be unbuffered.
-        kwargs['stdout'] = subprocess.PIPE
-    logging.log(log_level, log_msg)
+def run(
+    cmd,
+    verbose=True,
+    cwd=None,
+    check=True,
+    capture_output=False,
+    encoding="utf-8",
+    log_level=logging.DEBUG,
+    # Only pipe stdout; we want stderr (logging, errors) to be unbuffered.
+    stdout=subprocess.PIPE,
+    # Specify an integer number of seconds
+    timeout=-1,
+    **kwargs,
+):
+    if verbose:
+        info = "$ "
+        if cwd is not None:
+            info += f"cd {cwd}; "
+        info += " ".join(shlex.quote(c) for c in cmd)
+        if capture_output:
+            info += " >& ..."
+        lines = textwrap.wrap(
+            info,
+            break_on_hyphens=False,
+            break_long_words=False,
+            replace_whitespace=False,
+            subsequent_indent="  ",
+        )
+        logging.log(log_level, " \\\n".join(lines))
+    if timeout != -1:
+        cmd = ["timeout", "--signal=KILL", f"{timeout}s", *cmd]
     try:
-        return subprocess.run(cmd, check=check, encoding=encoding, **kwargs)
-    except subprocess.CalledProcessError as exc:
-        logging.critical("%s", exc.stdout)
+        return subprocess.run(
+            cmd,
+            cwd=cwd,
+            check=check,
+            capture_output=capture_output,
+            encoding=encoding,
+            stdout=stdout,
+            **kwargs,
+        )
+    except subprocess.CalledProcessError as e:
+        if e.returncode == -9:
+            # Error code from `timeout` command signaling it had to be killed
+            raise TimeoutError("Command timed out", cmd)
+        logging.critical("%s", e.stdout)
         raise
 
 
