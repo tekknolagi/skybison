e4839be45a458ab134b3014e870cd3deecdb4a39
Max Bernstein <max@bernsteinbear.com>
2023-11-28 16:52:24 -0500

    Add RETURN_VALUE to test
    The bytecode needs to end with RETURN_VALUE otherwise the analysis does
    not work. This is normally guaranteed by the compiler.

--- runtime/bytecode-test.cpp
+++ runtime/bytecode-test.cpp
@@ -767,6 +767,7 @@ TEST_F(
   const byte bytecode[] = {
       LOAD_FAST,  2, LOAD_FAST,  1, LOAD_FAST,   0, STORE_FAST, 2,
       STORE_FAST, 1, STORE_FAST, 0, DELETE_FAST, 0,
+      RETURN_VALUE, 0,
   };
   Bytes code_code(&scope, runtime_->newBytesWithAll(bytecode));
   Object empty_tuple(&scope, runtime_->emptyTuple());
@@ -792,6 +793,7 @@ TEST_F(
       LOAD_FAST_REVERSE,  4, 0, 0, STORE_FAST_REVERSE, 2, 0, 0,
       STORE_FAST_REVERSE, 3, 0, 0, STORE_FAST_REVERSE, 4, 0, 0,
       DELETE_FAST,        0, 0, 0,
+      RETURN_VALUE, 0,
   };
   Object rewritten_bytecode(&scope, function.rewrittenBytecode());
   EXPECT_TRUE(isMutableBytesEqualsBytes(rewritten_bytecode, expected));
