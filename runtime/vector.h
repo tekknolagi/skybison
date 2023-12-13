a4530724cb6300e0fec7607870bc959e827dfd9e
Max Bernstein <max@bernsteinbear.com>
2023-12-12 22:05:10 -0500

    nit

--- runtime/vector.h
+++ runtime/vector.h
@@ -104,13 +104,6 @@ class Vector {
     grow(new_capacity);
   }
 
-  void initializeWith(T elem) {
-    end_ = end_storage_;
-    for (T* i = begin_; i != end_; i++) {
-      *i = elem;
-    }
-  }
-
   void clear() { end_ = begin_; }
 
   void push_back(const T& t) {  // NOLINT
